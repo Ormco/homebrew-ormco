89a8f7a57234b6c7206a7b9b59b9f840f35cf939
Vitaliy Illarionov <vitaliy.illarionov@ormco.com>
2020-05-28 14:54:22 +0300

    Added QT 5.14.2 formula.

---
+++ Formula/qt-5.14.2.rb
@@ -0,0 +1,116 @@
+# Patches for Qt must be at the very least submitted to Qt's Gerrit codereview
+# rather than their bug-report Jira. The latter is rarely reviewed by Qt.
+class Qt5142 < Formula
+    desc "Cross-platform application and UI framework"
+    homepage "https://www.qt.io/"
+    url "https://download.qt.io/official_releases/qt/5.14/5.14.2/single/qt-everywhere-src-5.14.2.tar.xz"
+    mirror "https://mirrors.dotsrc.org/qtproject/archive/qt/5.14/5.14.2/single/qt-everywhere-src-5.14.2.tar.xz"
+    mirror "https://mirrors.ocf.berkeley.edu/qt/archive/qt/5.14/5.14.2/single/qt-everywhere-src-5.14.2.tar.xz"
+    sha256 "c6fcd53c744df89e7d3223c02838a33309bd1c291fcb6f9341505fe99f7f19fa"
+
+    head "https://code.qt.io/qt/qt5.git", :branch => "dev", :shallow => false
+
+    bottle do
+      cellar :any
+      sha256 "235bbe918f05509380ba870b24a84e14cbac044b56ade7b824408ad11963de41" => :catalina
+      sha256 "356f2d8914429724dc0b98ab194b3c32417870008650342a21ddbb26c130743d" => :mojave
+      sha256 "78f577a236c2eee17e371ae5efe951b80e01f6e2a491b6922442b71e0a2cf3e6" => :high_sierra
+    end
+
+    keg_only "Qt 5 has CMake issues when linked"
+
+    depends_on "pkg-config" => :build
+    depends_on :xcode => :build
+    depends_on :macos => :sierra
+
+    uses_from_macos "bison"
+    uses_from_macos "flex"
+    uses_from_macos "sqlite"
+
+    # Fix build on 10.15.4 SDK, included in 5.15
+    # https://github.com/qt/qtwebengine/commit/5d2026cb04ef8fd408e5722a84e2affb5b9a3119
+    patch :DATA
+
+    def install
+      args = %W[
+        -verbose
+        -prefix #{prefix}
+        -release
+        -opensource -confirm-license
+        -system-zlib
+        -qt-libpng
+        -qt-libjpeg
+        -qt-freetype
+        -qt-pcre
+        -nomake examples
+        -nomake tests
+        -no-rpath
+        -pkg-config
+        -dbus-runtime
+        -proprietary-codecs
+      ]
+
+      system "./configure", *args
+
+      # Remove reference to shims directory
+      inreplace "qtbase/mkspecs/qmodule.pri",
+                /^PKG_CONFIG_EXECUTABLE = .*$/,
+                "PKG_CONFIG_EXECUTABLE = #{Formula["pkg-config"].opt_bin/"pkg-config"}"
+      system "make"
+      ENV.deparallelize
+      system "make", "install"
+
+      # Some config scripts will only find Qt in a "Frameworks" folder
+      frameworks.install_symlink Dir["#{lib}/*.framework"]
+
+      # The pkg-config files installed suggest that headers can be found in the
+      # `include` directory. Make this so by creating symlinks from `include` to
+      # the Frameworks' Headers folders.
+      Pathname.glob("#{lib}/*.framework/Headers") do |path|
+        include.install_symlink path => path.parent.basename(".framework")
+      end
+
+      # Move `*.app` bundles into `libexec` to expose them to `brew linkapps` and
+      # because we don't like having them in `bin`.
+      # (Note: This move breaks invocation of Assistant via the Help menu
+      # of both Designer and Linguist as that relies on Assistant being in `bin`.)
+      libexec.mkpath
+      Pathname.glob("#{bin}/*.app") { |app| mv app, libexec }
+    end
+
+    def caveats
+      <<~EOS
+        We agreed to the Qt open source license for you.
+        If this is unacceptable you should uninstall.
+      EOS
+    end
+
+    test do
+      (testpath/"hello.pro").write <<~EOS
+        QT       += core
+        QT       -= gui
+        TARGET = hello
+        CONFIG   += console
+        CONFIG   -= app_bundle
+        TEMPLATE = app
+        SOURCES += main.cpp
+      EOS
+
+      (testpath/"main.cpp").write <<~EOS
+        #include <QCoreApplication>
+        #include <QDebug>
+        int main(int argc, char *argv[])
+        {
+          QCoreApplication a(argc, argv);
+          qDebug() << "Hello World!";
+          return 0;
+        }
+      EOS
+
+      system bin/"qmake", testpath/"hello.pro"
+      system "make"
+      assert_predicate testpath/"hello", :exist?
+      assert_predicate testpath/"main.o", :exist?
+      system "./hello"
+    end
+  end
